ded354aa163a6077b0803e26ae098650c57bd2cd
Emir Puska <emir.puska@it-akademija.com>
2025-05-29 20:43:14 +0200

    StaxPictureWriter implemented

--- src/main/java/com/itakademija/paint/xml/stax/StaxPictureReader.java
+++ src/main/java/com/itakademija/paint/xml/stax/StaxPictureReader.java
@@ -18,92 +18,84 @@ public class StaxPictureReader implements PictureReader {
     public List<PaintShape> read(String picturePath) {
         try {
             List<PaintShape> paintShapes = new ArrayList<>();
+            //StaX
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
             XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileReader(picturePath));
+            boolean openX = false;
+            String x = "";
+            boolean openY = false;
+            String y = "";
+            boolean openWidth = false;
+            String width;
+            boolean openHeight = false;
+            String height;
+            boolean openColor = false;
+            String color = "";
+            boolean openPaint = false;
+            String paint = "";
             while (xmlStreamReader.hasNext()) {
-                //otvarajući element neki( <x> <y>, <color>..
-                // content ili sadržaj od x, y, color
-                //zatvarajući neki (</x>, </y>..)
                 int typeOfElement = xmlStreamReader.next();
-                PaintShape paintShape = mapToPaintShape(xmlStreamReader, typeOfElement);
-                paintShapes.add(paintShape);
+                switch (typeOfElement) {
+                    case XMLStreamReader.START_ELEMENT:
+                        if ("x".equals(xmlStreamReader.getName().toString())) {
+                            openX = true;
+                        } else if ("y".equals(xmlStreamReader.getName().toString())) {
+                            openY = true;
+                        } else if ("width".equals(xmlStreamReader.getName().toString())) {
+                            openWidth = true;
+                        } else if ("height".equals(xmlStreamReader.getName().toString())) {
+                            openHeight = true;
+                        } else if ("color".equals(xmlStreamReader.getName().toString())) {
+                            openColor = true;
+                        } else if ("paint".equals(xmlStreamReader.getName().toString())) {
+                            openPaint = true;
+                        }
+                        break;
+                    case XMLStreamReader.CHARACTERS:
+                        if (openX) {
+                            x = xmlStreamReader.getText();
+                        } else if (openY) {
+                            y = xmlStreamReader.getText();
+                        } else if (openWidth) {
+                            width = xmlStreamReader.getText();
+                        } else if (openHeight) {
+                            height = xmlStreamReader.getText();
+                        } else if (openColor) {
+                            color = xmlStreamReader.getText();
+                        } else if (openPaint) {
+                            paint = xmlStreamReader.getText();
+                        }
+                        break;
+                    case XMLStreamReader.END_ELEMENT:
+                        if ("x".equals(xmlStreamReader.getName().toString())) {
+                            openX = false;
+                        } else if ("y".equals(xmlStreamReader.getName().toString())) {
+                            openY = false;
+                        } else if ("width".equals(xmlStreamReader.getName().toString())) {
+                            openWidth = false;
+                        } else if ("height".equals(xmlStreamReader.getName().toString())) {
+                            openHeight = false;
+                        } else if ("color".equals(xmlStreamReader.getName().toString())) {
+                            openColor = false;
+                        } else if ("paint".equals(xmlStreamReader.getName().toString())) {
+                            openPaint = false;
+                        } else if ("shape".equalsIgnoreCase(xmlStreamReader.getName().toString())) {
+                            Color colorAwt = "BLUE".equalsIgnoreCase(color) ? Color.BLUE : Color.RED;
+                            PaintShape paintShape;
+                            if ("ellipse".equalsIgnoreCase(paint)) {
+                                paintShape = new Ellipse(Integer.parseInt(x), Integer.parseInt(y), colorAwt);
+                            } else {
+                                paintShape = new Rectangle(Integer.parseInt(x), Integer.parseInt(y), colorAwt);
+                            }
+                            paintShapes.add(paintShape);
+                        }
+                        break;
+                }
             }
             return paintShapes;
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         return Collections.emptyList();
-    }
-    
-    
-    private PaintShape mapToPaintShape(XMLStreamReader xmlStreamReader, int typeOfElement){
-        boolean openX = false;
-        String x = "";
-        boolean openY = false;
-        String y = "";
-        boolean openWidth = false;
-        String width;
-        boolean openHeight = false;
-        String height;
-        boolean openColor = false;
-        String color = "";
-        boolean openPaint = false;
-        String paint = "";
-        PaintShape paintShape = null;
-        switch (typeOfElement) {
-            case XMLStreamReader.START_ELEMENT:
-                if ("x".equals(xmlStreamReader.getName().toString())) {
-                    openX = true;
-                } else if ("y".equals(xmlStreamReader.getName().toString())) {
-                    openY = true;
-                } else if ("width".equals(xmlStreamReader.getName().toString())) {
-                    openWidth = true;
-                } else if ("height".equals(xmlStreamReader.getName().toString())) {
-                    openHeight = true;
-                } else if ("color".equals(xmlStreamReader.getName().toString())) {
-                    openColor = true;
-                } else if ("paint".equals(xmlStreamReader.getName().toString())) {
-                    openPaint = true;
-                }
-                break;
-            case XMLStreamReader.CHARACTERS:
-                if (openX) {
-                    x = xmlStreamReader.getText();
-                } else if (openY) {
-                    y = xmlStreamReader.getText();
-                } else if (openWidth) {
-                    width = xmlStreamReader.getText();
-                } else if (openHeight) {
-                    height = xmlStreamReader.getText();
-                } else if (openColor) {
-                    color = xmlStreamReader.getText();
-                } else if (openPaint) {
-                    paint = xmlStreamReader.getText();
-                }
-                break;
-            case XMLStreamReader.END_ELEMENT:
-                if ("x".equals(xmlStreamReader.getName().toString())) {
-                    openX = false;
-                } else if ("y".equals(xmlStreamReader.getName().toString())) {
-                    openY = false;
-                } else if ("width".equals(xmlStreamReader.getName().toString())) {
-                    openWidth = false;
-                } else if ("height".equals(xmlStreamReader.getName().toString())) {
-                    openHeight = false;
-                } else if ("color".equals(xmlStreamReader.getName().toString())) {
-                    openColor = false;
-                } else if ("paint".equals(xmlStreamReader.getName().toString())) {
-                    openPaint = false;
-                } else if ("shape".equalsIgnoreCase(xmlStreamReader.getName().toString())) {
-                    Color colorAwt = "BLUE".equalsIgnoreCase(color) ? Color.BLUE : Color.RED;
-                    if ("ellipse".equalsIgnoreCase(paint)) {
-                        paintShape = new Ellipse(Integer.parseInt(x), Integer.parseInt(y), colorAwt);
-                    } else {
-                        paintShape = new Rectangle(Integer.parseInt(x), Integer.parseInt(y), colorAwt);
-                    }
-                }
-                break;
-        }
-        return paintShape;
     }
 }
--- src/main/java/com/itakademija/paint/xml/stax/StaxPictureWriter.java
+++ src/main/java/com/itakademija/paint/xml/stax/StaxPictureWriter.java
@@ -1,13 +1,57 @@
 package com.itakademija.paint.xml.stax;
 
+import com.itakademija.paint.shape.Ellipse;
 import com.itakademija.paint.shape.PaintShape;
 import com.itakademija.paint.xml.PictureWriter;
 
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.util.List;
 
 public class StaxPictureWriter implements PictureWriter {
     @Override
     public void write(String picturePath, List<PaintShape> paintShapes) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
+            XMLStreamWriter xmlStreamWriter = xmlOutputFactory.createXMLStreamWriter(new FileWriter(picturePath));
+            xmlStreamWriter.writeStartDocument();
+            xmlStreamWriter.writeStartElement("shapes");
+            for (PaintShape paintShape : paintShapes) {
+                xmlStreamWriter.writeStartElement("shape");
+                //x
+                xmlStreamWriter.writeStartElement("x");
+                xmlStreamWriter.writeCharacters(paintShape.getX() + "");
+                xmlStreamWriter.writeEndElement();
+                //y
+                xmlStreamWriter.writeStartElement("y");
+                xmlStreamWriter.writeCharacters(paintShape.getY() + "");
+                xmlStreamWriter.writeEndElement();
+                //width
+                xmlStreamWriter.writeStartElement("width");
+                xmlStreamWriter.writeCharacters(paintShape.getWidth() + "");
+                xmlStreamWriter.writeEndElement();
+                //height
+                xmlStreamWriter.writeStartElement("height");
+                xmlStreamWriter.writeCharacters(paintShape.getHeight() + "");
+                xmlStreamWriter.writeEndElement();
+                //color
+                xmlStreamWriter.writeStartElement("color");
+                xmlStreamWriter.writeCharacters(paintShape.getColor().equals(Color.BLUE) ? "blue" : "red");
+                xmlStreamWriter.writeEndElement();
+                //paint
+                xmlStreamWriter.writeStartElement("paint");
+                xmlStreamWriter.writeCharacters(paintShape instanceof Ellipse ? "ellipse" : "rectangle");
+                xmlStreamWriter.writeEndElement();
+                xmlStreamWriter.writeEndElement();
+            }
+            xmlStreamWriter.writeEndElement();
+            xmlStreamWriter.writeEndDocument();
+            xmlStreamWriter.flush();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
